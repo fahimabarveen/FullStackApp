34a599ba1e88b921e687aa14a7a0bbb480cbc0ec
Fahima <fahimasherinaz@gmail.com>
2024-12-30 17:39:15 +0000

    clone

--- frontend/app/_components/Header.jsx
+++ frontend/app/_components/Header.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 import Image from 'next/image';
 import { LayoutGrid, Search,LogInIcon, ShoppingBagIcon } from 'lucide-react';
 import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 function Header() {
   return (
@@ -12,11 +20,24 @@ function Header() {
         </Image>    
        </div>
         
-        <div className='hidden md:flex gap-2 items-center'>
-         <h2 className='flex gap-2 items-center border rounded-full p-2 px-10 bg-blue-400'>
-          <LayoutGrid className='h-5 w-5'/>Categories
-         </h2>
-         </div>
+      <div className='hidden md:flex gap-2 items-center'>
+         
+        <DropdownMenu>
+          <DropdownMenuTrigger><h2 className='flex gap-2 items-center border rounded-full p-2 px-10 bg-blue-400'>
+           <LayoutGrid className='h-5 w-5'/>Categories</h2>
+            </DropdownMenuTrigger>
+             <DropdownMenuContent>
+              <DropdownMenuLabel>Product Categories</DropdownMenuLabel>
+               <DropdownMenuSeparator />
+                <DropdownMenuItem>Profile</DropdownMenuItem>
+                <DropdownMenuItem>Billing</DropdownMenuItem>
+                <DropdownMenuItem>Team</DropdownMenuItem>
+                <DropdownMenuItem>Subscription</DropdownMenuItem>
+             </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+         
+
 
         <div className='hidden md:flex gap-2 items-center'>
           <Search/>
@@ -28,13 +49,13 @@ function Header() {
         </div>
         
         <div className='flex gap-2 items-center m1-auto'>
-
-        </div>
-        <div>
-          <h2> <ShoppingBagIcon className='h-5 w-5'/>0</h2>
+      
+         <div>
+          <h2><ShoppingBagIcon className='h-5 w-5'/>0</h2>
            <Button className='bg-gradient-to-r from-teal-400 to-blue-500 hover:from-pink-500 hover:to-orange-500'>
             <LogInIcon/>LOGIN
            </Button>
+         </div>
         </div>
     </div>
   );
